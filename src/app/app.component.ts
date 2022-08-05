4db23eb29d007119370aecea7cf1b70c6eef96d9
Your Name <you@example.com>
2022-08-05 15:47:42 +0530

    changes done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, fromEvent, interval, switchMap, switchScan } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project';
+  counter = 0;
+  searchControl: FormControl = new FormControl();
+
+  ngOnInit() {
+    // fromEvent(document, 'click').pipe(
+    //   switchMap(() => interval(500))
+    // ).subscribe((val) => (this.counter = val));
+
+    this.searchControl.valueChanges.pipe(
+      debounceTime(1000),
+      switchMap(() => interval(500))
+    )
+    .subscribe((val) => (this.counter = val));
+  }
 }
